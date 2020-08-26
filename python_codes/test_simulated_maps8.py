a8f4af9eb85eba3850ee403a272d8041fcd5179d
cmdoret <cyril.mattheydoret@gmail.com>
2020-08-26 18:08:26 +0200

    add comments, remove unneeded steps, check for out of bounds instead of padding, to spare memory

--- python_codes/test_simulated_maps8.py
+++ python_codes/test_simulated_maps8.py
@@ -9,33 +9,39 @@ import itertools
 import os
 import sys
 import time
+from pathlib import Path
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
-import statsmodels.api as sm
 import cooler
+import hicstuff.hicstuff as hcs
 import chromosight.utils.preprocessing as cup
 import chromosight.kernels as ck
 
 # Parse CLI arguments
 # Path to the cool file with experimental data
 clr = cooler.Cooler(sys.argv[1])
-# Path to positions of borders in the experimental matrix (in bins)
-borders_pos = np.loadtxt(sys.argv[2])
+# Read selected chromosome
+chrom = sys.argv[2]
+# Path to positions of borders in the experimental matrix (in bins,
+# relative to chromosome start)
+borders_pos = np.loadtxt(sys.argv[3])
+# Create output directory if it does not exist already
+out_dir = Path(sys.argv[4])
+out_dir.mkdir(parents=True, exist_ok=True)
 
-
-lowess = sm.nonparametric.lowess
-
-# Experimental data:
-m = clr.matrix(balance=True, sparse=False)[:]
-bins = clr.bins[:]
+# Load experimental (balanced !) Hi-C data:
+# To balance it, you can use cooler's CLI command on the file: cooler balance
+mat = clr.matrix(balance=True, sparse=False).fetch(chrom)
+# Load genomic coordinates of bins (make indices relative to chromosome)
+bins = clr.bins().fetch(chrom).reset_index(drop=True)
+# Retrieve missing bins that were excluded from the normalisation
 bad_bins = bins.loc[np.isnan(bins.weight)].index.values
-Nreads = sum(m)
+# Number of raw contacts in raw the matrix
+nreads = clr.matrix(sparse=True, balance=False).fetch(chrom).sum()
+# Desired number of simulated matrices to generate
 Nrealisations = 10
-n1 = shape(m)[0]  # size of the chromosome contact map
-
-# Normalisation:
-
+n = m.shape[0]  # size of the chromosome contact map
 
 # Load patterns
 # Generic pattern:
@@ -45,44 +51,41 @@ ratio_loops = ck.loops["kernels"][0]
 ratio_loops = ratio_loops ** 0.7  # to attenuate a bit
 
 # Remove borders from experimental data overlapping empty bins:
-chrm = 5
-border_pos = list(borders_pos)
+borders_pos = list(borders_pos)
 for b in borders_pos.copy():
     # If any bad (empty) bin is closer to a border than the radius of the
     # pattern template, drop it
     if np.any(np.abs(bad_bins - b) < max(ratio_borders.shape)) in b:
         borders_pos.remove(b)
-# To convert the 1D vetor into matrice object:
-mat_indices = np.zeros((shape(m)))
+
+# To convert the 1D vector into matrice object:
+mat_indices = np.zeros(mat.shape)
 compt = 0
 vect_compt = []
-for i, j in itertools.product(range(n1), range(n1)):
+for i, j in itertools.product(range(n), range(n)):
     mat_indices[i, j] = compt
     vect_compt.append((i, j))
     compt += 1
 
-#  Genomic distance law from experiemental data:
-s = shape(matscn1)[0]
-prob_d = distance_law_human.dist_law(matscn1)
-prob_d = prob_d / sum(prob_d)
-
-# LOWESS Fit:
-z = lowess(prob_d[:, 0], range(0, len(prob_d)), frac=1.0 / 15)
-prob_d_lowess = z[:, 1]
+#  Genomic distance law from experimental data:
+d_smooth = cup.distance_law(
+    clr.matrix(sparse=True, balance=True).fetch(chrom).tocsr(),
+    detectable_bins=~np.isnan(bins.weight),
+    smooth=True,
+)
+prob_d_smooth = d_smooth / np.sum(d_smooth)
 
 # TADs size distribution:
-list_size = []
-for i in range(1, len(b)):
-    bi = b[i] - b[i - 1]
-    list_size.append(bi)
-
-s = shape(matscn1)[0]
+borders_sizes = []
+for i in range(1, len(borders_pos)):
+    bi = borders_pos[i] - borders_pos[i - 1]
+    borders_sizes.append(bi)
 
 # Genomic distance law:
-mat_geno = np.zeros((shape(m)))
-for i in range(0, s):
-    for j in range(0, s):
-        mat_geno[i, j] = prob_d_lowess[abs(j - i)]
+mat_geno = np.zeros(mat.shape)
+for i in range(0, n):
+    for j in range(0, n):
+        mat_geno[i, j] = prob_d_smooth[abs(j - i)]
 
 if len(mat_geno[mat_geno < 0]) > 0:
     raise ValueError(
@@ -102,14 +105,16 @@ for random_i in range(1, Nrealisations):
     # Random borders generation
     borders_random = []
     p = 0
-    for i in range(0, len(b)):
+    for i in range(0, len(borders_pos)):
         sep = int(
             np.random.normal(
-                loc=mean(list_size), scale=std(list_size), size=None
+                loc=np.mean(borders_sizes),
+                scale=np.std(borders_sizes),
+                size=None,
             )
         )
         p = p + sep
-        if p < n1:
+        if p < n:
             borders_random.append(p)
     np.savetxt(
         "Borders_realisation" + "_" + str(random_i) + ".txt",
@@ -118,98 +123,95 @@ for random_i in range(1, Nrealisations):
     )
 
     # Adding of CIDs/TADs borders:--------------------------------------------
-    mat_borders = np.ones((shape(m)))
-    area = int(shape(ratio_borders)[0] / 2)
-    mat_borders = np.concatenate(
-        (mat_borders, mat_borders, mat_borders), axis=1
-    )
-    mat_borders = np.concatenate(
-        (mat_borders, mat_borders, mat_borders), axis=0
-    )
+    mat_borders = np.ones(mat.shape)
+    area = int(ratio_borders.shape[0] // 2)
     nb = 0
     for bi in borders_random:
         bi = int(bi)
         nb += 1
-        mat_borders_i = np.ones((shape(mat_borders)))
+        if bi > (n - area) or bi <= area:
+            continue
+        mat_borders_i = np.ones(mat_borders.shape)
         mat_borders_i[
             np.ix_(
-                range(n1 + bi - area, n1 + bi + area + 1),
-                range(n1 + bi - area, n1 + bi + area + 1),
+                range(n + bi - area, n + bi + area + 1),
+                range(n + bi - area, n + bi + area + 1),
             )
         ] = ratio_borders
-        mat_borders = mat_borders * mat_borders_i
-    mat_borders = mat_borders[np.ix_(range(n1, 2 * n1), range(n1, 2 * n1))]
+        mat_borders *= mat_borders_i
     mat_borders = (
         mat_borders + np.transpose(mat_borders)
     ) / 2  #  resymetrisation
 
-    # Adding of LOOPS patterns:  ----------------------------------------------
+    # Adding of loops patterns:  ----------------------------------------------
     combi_pos = list(itertools.combinations(borders_random, 2))
     loops_random = random.sample(combi_pos, 10)
 
     loops_random = []
     for b1 in borders_random:
         for b2 in borders_random:
-            if b2 - b1 > 2 and b2 - b1 < 100:  #  loops detectable until 200 kb
-                if (100 - (b2 - b1)) / 100.0 > rand() * 5.0:
+            lsize = b2 - b1
+            #  loops detectable between 4 and 200 kb
+            if 4000 / clr.binsize > lsize < 200000 / clr.binsize:
+                # Give a probability of using each loop inversely proportional
+                # to the length of the loop (-> more small loops)
+                if (100 - lsize) / 100.0 > np.random.rand() * 5.0:
                     loops_random.append((b1, b2))
-                    print(b2 - b1)
     loops_random = set(loops_random)
     np.savetxt(
         "Loops_realisation" + "_" + str(random_i) + ".txt",
         list(loops_random),
         fmt="%d",
     )
-    mat_loops = np.ones((shape(m)))
-    area = int(shape(ratio_loops)[0] / 2)
-    mat_loops = np.concatenate((mat_loops, mat_loops, mat_loops), axis=1)
-    mat_loops = np.concatenate((mat_loops, mat_loops, mat_loops), axis=0)
+    mat_loops = np.ones(mat.shape)
+    area = int(ratio_loops.shape[0] // 2)
     for l in loops_random:
-        mat_loops_i = np.ones((shape(mat_loops)))
+        l = np.array(l).astype(int)
+        # Check if either loop anchor goes out of the map
+        if np.any(l > (n - area)) or np.any(l <= area):
+            continue
+        mat_loops_i = np.ones(mat_loops.shape)
+        # Note we fill the upper triangle (l[0] < l[1])
         mat_loops_i[
             np.ix_(
-                range(n1 + int(l[0]) - area, n1 + int(l[0]) + area + 1),
-                range(n1 + int(l[1]) - area, n1 + int(l[1]) + area + 1),
+                range(n + l[0] - area, n + l[0] + area + 1),
+                range(n + l[1] - area, n + l[1] + area + 1),
             )
         ] = ratio_loops
         mat_loops = mat_loops * mat_loops_i
-        mat_loops_i[
-            np.ix_(
-                range(n1 + int(l[1]) - area, n1 + int(l[1]) + area + 1),
-                range(n1 + int(l[0]) - area, n1 + int(l[0]) + area + 1),
-            )
-        ] = np.transpose(ratio_loops)
-        mat_loops = mat_loops * mat_loops_i
+        mat_loops *= mat_loops_i
+    # Set lower triangle to 0
+    mat_loops = np.triu(mat_loops)
+    # Make matrix symmetric by adding upper triangle to lower triangle
+    mat_loops = mat_loops + np.transpose(mat_loops)
+    # Remove half of diagonal since it has been doubled during transpose
+    mat_loops[np.diag_indices_from(mat_loops)] /= 2
 
-    mat_loops = mat_loops[
-        np.ix_(range(n1, 2 * n1), range(n1, 2 * n1))
-    ]  #  we refocus
-    mat_loops = (mat_loops + np.transpose(mat_loops)) / 2  #  resymetrisation
-    #    imshow(mat_loops, vmin=0.0, vmax = 2.0, interpolation ="none", cmap = "seismic")
-    #    colorbar()
-
-    # Complete Propensities Map with all features :
-    #    mat_propen = mat_geno * mat_borders
-    #    mat_propen = mat_geno * mat_loops
+    # The final propensity map will be the (elementwise) product of the genomic
+    # distance law map, the border map and the loop map:
     mat_propen = mat_geno * mat_borders * mat_loops
-    mat_propen = scn.scn_func(mat_propen, 0)
+    mat_propen = hcs.normalize_dense(mat_propen, iterations=40)
 
-    # Conversion of the propensities matrice into a vector :
-    vect_propen = np.reshape(mat_propen, (s * s, 1))
+    # Conversion of the propensities matrice into a 1D vector :
+    vect_propen = np.reshape(mat_propen, (n * n, 1))
+    # Convert to probabilities
     vect_propen = vect_propen * 1.0 / sum(vect_propen)
     vect_propen = np.squeeze(vect_propen)
-    vect_indices = range(0, s * s)
+    vect_indices = range(0, n * n)
+    # Generate a distribution from the vector  of probabilities and sample contacts
     custm = stats.rv_discrete(name="custm", values=(vect_indices, vect_propen))
+    nreads2 = int(nreads / 2)
+    vect_realisation = custm.rvs(size=nreads2)  #  time consuming !!
 
-    Nreads2 = int(Nreads / 2)
-    vect_realisation = custm.rvs(size=Nreads2)  #  time consuming !!
-
-    # Come back to the matrice:
-    mat_simul = np.zeros((shape(m)))
+    # Come back to the matrice and fill with sampled contacts:
+    mat_simul = np.zeros(mat.shape)
     for v in vect_realisation:
         (i, j) = vect_compt[v]
         mat_simul[i, j] += 1
-        mat_simul[j, i] += 1
+
+    # Again, symmetrise matrix, accounting for doubled diagonal
+    mat_simul += np.transpose(mat_simul)
+    mat_loops[np.diag_indices_from(mat_loops)] /= 2
 
     np.savetxt(
         "MAT_RAW_realisation" + "_" + str(random_i) + ".txt",
@@ -217,7 +219,7 @@ for random_i in range(1, Nrealisations):
         fmt="%d",
     )
 
-    matscn2 = scn.scn_func(mat_simul, 0)
+    matscn2 = hcs.normalize_dense(mat_simul, iterations=30)
     matscn2 = (matscn2 + np.transpose(matscn2)) / 2.0
     np.savetxt(
         "MAT_NORMALISED_realisation" + "_" + str(random_i) + ".txt",
@@ -225,7 +227,7 @@ for random_i in range(1, Nrealisations):
         fmt="%e",
     )
 
-    imshow(
+    plt.imshow(
         matscn2 ** 0.15,
         interpolation="none",
         cmap="afmhot_r",
